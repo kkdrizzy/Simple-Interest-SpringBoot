22e3e50c26adf871c850ad3d4e5455d8ee3e2b0e
kkdrizzy <kaustubhkatti.205@gmail.com>
2022-02-22 15:25:34 +0530

    Swagger commit

--- src/main/java/com/example/SimpleInterest/SimpleInterestApplication.java
+++ src/main/java/com/example/SimpleInterest/SimpleInterestApplication.java
@@ -2,12 +2,26 @@ package com.example.SimpleInterest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class SimpleInterestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleInterestApplication.class, args);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.SimpleInterest")).build();
     }
 
 }
